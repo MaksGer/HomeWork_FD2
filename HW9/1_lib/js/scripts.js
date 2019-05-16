84b24613eaf9eca2ba58f64b4de0bad0a29f6db0
MaksGer <48175351+MaksGer@users.noreply.github.com>
2019-05-16 06:49:36 +0200

    Add files via upload

---
+++ HW9/1_lib/js/scripts.js
@@ -0,0 +1,31 @@
+window.onload = function(e) {
+
+    const inputs = document.querySelectorAll('.check'),
+        button = document.querySelector('.button');
+    button.addEventListener('click', () => {
+        let error = false;
+
+        for (let i = 0; i < inputs.length; i++) {
+            if (inputs[i].value === '') {
+                inputs[i].classList.add('err');
+                error = true;
+            }
+        }
+
+
+    });
+    let jqInputs = $('.check');
+
+    jqInputs.on('click', function () {
+        this.classList.remove('err');
+    });
+
+    jqInputs.on('focus', function () {
+        this.classList.add('focus');
+    });
+
+    jqInputs.on('blur', function (e) {
+        this.classList.remove('focus');
+        jqInputs.validation();
+    });
+};
